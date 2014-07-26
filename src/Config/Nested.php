7771356e9301b2d3bab3174d91c9d1f57f412cba
Kirill Popov <kirill@ecomarkup.com>
2014-07-26 16:34:04 +0400

    fromCamelCase() method added

--- src/Config/Nested.php
+++ src/Config/Nested.php
@@ -77,4 +77,18 @@ abstract class Nested {
 		}
 		return true;
 	}
+
+	/**
+	 * @param $input
+	 * @param string $separator
+	 * @return string
+	 */
+	public static function fromСamelСase($input, $separator = '_') {
+		preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
+		$ret = $matches[0];
+		foreach ($ret as &$match) {
+			$match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+		}
+		return implode($separator, $ret);
+	}
 }
